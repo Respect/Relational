21b4a7651825e87ead97d29e181e2842fbfc6d6d
Henrique Moody <henriquemoody@gmail.com>
2012-03-17 01:06:51 -0300

    Created "North Wind" database style.

---
+++ library/Respect/Relational/Styles/NorthWind.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Respect\Relational\Styles;
+
+class NorthWind extends Standard
+{
+
+    public function entityToTable($name)
+    {
+        return $name;
+    }
+
+    public function tableToEntity($name)
+    {
+        return $name;
+    }
+
+    public function manyFromLeftRight($left, $right)
+    {
+        $left = $this->pluralToSingular($left);
+        return "{$left}{$right}";
+    }
+
+    public function manyFromRightLeft($right, $left)
+    {
+        $right = $this->pluralToSingular($right);
+        return "{$right}{$left}";
+    }
+
+    public function primaryFromTable($name)
+    {
+        return $this->pluralToSingular($name) . 'ID';
+    }
+    
+    public function foreignFromTable($name)
+    {
+        return $this->pluralToSingular($name) . 'ID';
+    }
+
+    
+
+}
+
---
+++ tests/library/Respect/Relational/Styles/NorthWindTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Respect\Relational\Styles;
+
+class NorthWindTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Respect\Relational\Styles\NorthWind
+     */
+    private $style;
+
+
+    public function tableEntityProvider()
+    {
+        return array(
+            array('Posts',              'Posts'),
+            array('Comments',           'Comments'),
+            array('Categories',         'Categories'),
+            array('PostCategories',     'PostCategories'),
+            array('PostTags',           'PostTags'),
+        );
+    }
+
+    public function manyToMantTableProvider()
+    {
+        return array(
+            array('Posts',  'Categories',   'PostCategories'),
+            array('Users',  'Groups',       'UserGroups'),
+            array('Groups', 'Profiles',     'GroupProfiles'),
+        );
+    }
+
+    public function columnsPropertyProvider()
+    {
+        return array(
+            array('GroupID'),
+            array('PostID'),
+            array('CreatorID'),
+            array('Text'),
+            array('Created'),
+        );
+    }
+    
+    public function keyProvider()
+    {
+        return array(
+            array('Posts',      'PostID'),
+            array('Authors',    'AuthorID'),
+            array('Tags',       'TagID'),
+            array('Users',      'UserID'),
+        );
+    }
+
+
+    public function setUp()
+    {
+        $this->style = new NorthWind();
+    }
+
+    public function tearDown()
+    {
+        $this->style = null;
+    }
+
+    /**
+     * @dataProvider tableEntityProvider
+     */
+    public function test_table_and_entities_methods($table, $entity)
+    {
+        $this->assertEquals($entity, $this->style->tableToEntity($table));
+        $this->assertEquals($table, $this->style->entityToTable($entity));
+    }
+
+    /**
+     * @dataProvider columnsPropertyProvider
+     */
+    public function test_columns_and_properties_methods($column)
+    {
+        $this->assertEquals($column, $this->style->columnToProperty($column));
+        $this->assertEquals($column, $this->style->propertyToColumn($column));
+    }
+
+
+    /**
+     * @dataProvider manyToMantTableProvider
+     */
+    public function test_table_from_right_left_table($right, $left, $table)
+    {
+        $this->assertEquals($table, $this->style->manyFromRightLeft($right, $left));
+    }
+
+
+    /**
+     * @dataProvider manyToMantTableProvider
+     */
+    public function test_table_from_left_right_table($left, $right, $table)
+    {
+        $this->assertEquals($table, $this->style->manyFromLeftRight($left, $right));
+    }
+    
+    /**
+     * @dataProvider keyProvider
+     */
+    public function test_keys($table, $foreign)
+    {
+        $this->assertEquals($foreign, $this->style->primaryFromTable($table));
+        $this->assertEquals($foreign, $this->style->foreignFromTable($table));
+    }
+
+}
+
