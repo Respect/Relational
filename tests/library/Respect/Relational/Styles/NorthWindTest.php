85256d5308494e4ac54ecceb8757be42ebb0683e
Henrique Moody <henriquemoody@gmail.com>
2012-03-18 21:31:51 -0300

    Created test for the NorthWind database style with a real database.

--- tests/library/Respect/Relational/Styles/NorthWindTest.php
+++ tests/library/Respect/Relational/Styles/NorthWindTest.php
@@ -1,6 +1,12 @@
 <?php
 
-namespace Respect\Relational\Styles;
+namespace Respect\Relational\Styles\NorthWind;
+
+use PDO,
+    Respect\Relational\Db,
+    Respect\Relational\Sql,
+    Respect\Relational\Styles\NorthWind,
+    Respect\Relational\Mapper;
 
 class NorthWindTest extends \PHPUnit_Framework_TestCase
 {
@@ -10,6 +16,139 @@ class NorthWindTest extends \PHPUnit_Framework_TestCase
      */
     private $style;
 
+    /**
+     * @var Respect\Relational\Mapper
+     */
+    private $mapper;
+    
+    /**
+     * @var PDO
+     */
+    private $conn;
+
+    public function setUp()
+    {
+
+        $conn = new PDO('sqlite::memory:');
+        $db = new Db($conn);
+        $conn->exec(
+            (string) Sql::createTable(
+                'Posts',
+                array(
+                    'PostID INTEGER PRIMARY KEY',
+                    'Title VARCHAR(255)',
+                    'Text TEXT',
+                    'AuthorID INTEGER',
+                )
+            )
+        );
+        $conn->exec(
+            (string) Sql::createTable(
+                'Authors',
+                array(
+                    'AuthorID INTEGER PRIMARY KEY',
+                    'Name VARCHAR(255)'
+                )
+            )
+        );
+        $conn->exec(
+            (string) Sql::createTable(
+                'Comments',
+                array(
+                    'CommentID INTEGER PRIMARY KEY',
+                    'PostID INTEGER',
+                    'Text TEXT',
+                )
+            )
+        );
+
+        $conn->exec(
+            (string) Sql::createTable(
+                'Categories',
+                array(
+                    'CategoryID INTEGER PRIMARY KEY',
+                    'Name VARCHAR(255)',
+                    'Description TEXT'
+                )
+            )
+        );
+        $conn->exec(
+            (string) Sql::createTable(
+                'PostCategories',
+                array(
+                    'PostCategoryID INTEGER PRIMARY KEY',
+                    'PostID INTEGER',
+                    'CategoryID INTEGER'
+                )
+            )
+        );
+        $this->posts = array(
+            (object) array(
+                'PostID' => 5,
+                'Title' => 'Post Title',
+                'Text' => 'Post Text',
+                'AuthorID' => 1
+            )
+        );
+        $this->authors = array(
+            (object) array(
+                'AuthorID' => 1,
+                'Name' => 'Author 1'
+            )
+        );
+        $this->comments = array(
+            (object) array(
+                'CommentID' => 7,
+                'PostID' => 5,
+                'Text' => 'Comment Text'
+            ),
+            (object) array(
+                'CommentID' => 8,
+                'PostID' => 4,
+                'Text' => 'Comment Text 2'
+            )
+        );
+        $this->categories = array(
+            (object) array(
+                'CategoryID' => 2,
+                'Name' => 'Sample Category',
+                'Description' => 'Category description'
+            ),
+            (object) array(
+                'CategoryID' => 3,
+                'Name' => 'NONON',
+                'CategoryID' => null
+            )
+        );
+        $this->postsCategories = array(
+            (object) array(
+                'PostCategoryID' => 66,
+                'PostID' => 5,
+                'CategoryID' => 2
+            )
+        );
+
+        foreach ($this->authors as $author)
+            $db->insertInto('Authors', (array) $author)->values((array) $author)->exec();
+
+        foreach ($this->posts as $post)
+            $db->insertInto('Posts', (array) $post)->values((array) $post)->exec();
+
+        foreach ($this->comments as $comment)
+            $db->insertInto('Comments', (array) $comment)->values((array) $comment)->exec();
+
+        foreach ($this->categories as $category)
+            $db->insertInto('Categories', (array) $category)->values((array) $category)->exec();
+
+        foreach ($this->postsCategories as $postCategory)
+            $db->insertInto('PostCategories', (array) $postCategory)->values((array) $postCategory)->exec();
+
+        $this->conn     = $conn;
+        $this->style    = new NorthWind();
+        $this->mapper   = new Mapper($conn);
+        $this->mapper->setStyle($this->style);
+        $this->mapper->entityNamespace = __NAMESPACE__ . '\\';
+    }
 
     public function tableEntityProvider()
     {
@@ -52,17 +191,6 @@ class NorthWindTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-
-    public function setUp()
-    {
-        $this->style = new NorthWind();
-    }
-
-    public function tearDown()
-    {
-        $this->style = null;
-    }
-
     /**
      * @dataProvider tableEntityProvider
      */
@@ -102,5 +230,82 @@ class NorthWindTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($foreign, $this->style->foreignFromTable($table));
     }
 
+    public function test_fetching_entity_typed()
+    {
+        $mapper = $this->mapper;
+        $comment = $mapper->Comments[8]->fetch();
+        $this->assertInstanceOf(__NAMESPACE__ . '\Comments', $comment);
+    }
+
+    public function test_fetching_all_entity_typed()
+    {
+        $mapper = $this->mapper;
+        $comment = $mapper->Comments->fetchAll();
+        $this->assertInstanceOf(__NAMESPACE__ . '\Comments', $comment[1]);
+        
+        $categories = $mapper->PostCategories->Categories->fetch();
+        $this->assertInstanceOf(__NAMESPACE__ . '\PostCategories', $categories);
+        $this->assertInstanceOf(__NAMESPACE__ . '\Categories', $categories->CategoryID);
+    }
+
+    public function test_fetching_all_entity_typed_nested()
+    {
+        $mapper = $this->mapper;
+        $comment = $mapper->Comments->Posts->Authors->fetchAll();
+        $this->assertInstanceOf(__NAMESPACE__ . '\Comments', $comment[0]);
+        $this->assertInstanceOf(__NAMESPACE__ . '\Posts',    $comment[0]->PostID);
+        $this->assertInstanceOf(__NAMESPACE__ . '\Authors',  $comment[0]->PostID->AuthorID);
+    }
+
+    public function test_persisting_entity_typed()
+    {
+        $mapper = $this->mapper;
+        $comment = $mapper->Comments[8]->fetch();
+        $this->assertInstanceOf(__NAMESPACE__ . '\Comments', $comment);
+        $comment->Text = 'HeyHey';
+        $mapper->Comments->persist($comment);
+        $mapper->flush();
+        $result = $this->conn->query('select Text from Comments where CommentID=8')->fetchColumn(0);
+        $this->assertEquals('HeyHey', $result);
+    }
+
+    public function test_persisting_new_entity_typed()
+    {
+        $mapper = $this->mapper;
+        $comment = new Comments();
+        $comment->Text = 'HeyHey';
+        $mapper->Comments->persist($comment);
+        $mapper->flush();
+        $result = $this->conn->query('select Text from Comments where CommentID=9')->fetchColumn(0);
+        $this->assertEquals('HeyHey', $result);
+    }
+
+}
+
+
+
+class Posts
+{
+    public $PostID, $Title, $Text, $AuthorID;
+}
+
+class Authors
+{
+    public $AuthorID, $Name;
+}
+
+class Comments
+{
+    public $CommentID, $PostID, $Text;
+}
+
+class Categories
+{
+    public $CategoryID, $Name, $Description;
+}
+
+class PostCategories
+{
+    public $PostCategoryID, $PostID, $CategoryID;
 }
 
