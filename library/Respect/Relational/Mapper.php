d4e810071c4557894bd6799cabe1bf9671e009f6
Alexandre <alexandre@gaigalas.net>
2013-02-13 19:19:04 -0200

    Refactoring

--- library/Respect/Relational/Mapper.php
+++ library/Respect/Relational/Mapper.php
@@ -14,7 +14,11 @@ use Respect\Data\Collections\Collection;
 use Respect\Data\Collections as c;
 use Respect\Data\CollectionIterator;
 
-class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typable
+/** Maps objects to database operations */
+class Mapper extends AbstractMapper implements 
+    c\Filterable, 
+    c\Mixable, 
+    c\Typable
 {
     /** @var Respect\Relational\Db Holds our connector**/
     protected $db;
@@ -111,8 +115,9 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
         return $condition;
     }
 
-    protected function rawDelete(array $condition, Collection $collection, $entity)
-    {
+    protected function rawDelete(
+        array $condition, Collection $collection, $entity
+    ) {
         $name      = $collection->getName();
         $columns   = $this->extractColumns($entity, $collection);
         $condition = $this->guessCondition($columns, $collection);
@@ -135,16 +140,18 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
                     ->exec();
     }
 
-    protected function rawInsert(array $columns, Collection $collection, $entity = null)
-    {
+    protected function rawInsert(
+        array $columns, Collection $collection, $entity = null
+    ) {
         $name       = $collection->getName();
         $isInserted = $this->db
                             ->insertInto($name, $columns)
                             ->values($columns)
                             ->exec();
 
-        if (!is_null($entity))
+        if (!is_null($entity)) {
             $this->checkNewIdentity($entity, $collection);
+        }
 
         return $isInserted;
     }
@@ -153,6 +160,7 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
     {
         $conn = $this->db->getConnection();
         $conn->beginTransaction();
+        
         try {
             foreach ($this->changed as $entity)
                 $this->flushSingle($entity);
@@ -160,6 +168,7 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
             $conn->rollback();
             throw $e;
         }
+        
         $this->reset();
         $conn->commit();
     }
@@ -173,19 +182,24 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
             //some drivers may throw an exception here, it is just irrelevant
             return false;
         }
-        if (!$identity)
+        if (!$identity) {
             return false;
+        }
 
         $primaryName = $this->getStyle()->identifier($collection->getName());
         $entity->$primaryName = $identity;
         return true;
     }
 
-    protected function createStatement(Collection $collection, $withExtra = null)
-    {
+    protected function createStatement(
+        Collection $collection, $withExtra = null
+    ) {
         $query = $this->generateQuery($collection);
-        if ($withExtra instanceof Sql)
+        
+        if ($withExtra instanceof Sql) {
             $query->appendQuery($withExtra);
+        }
+        
         $statement = $this->db->prepare((string) $query, PDO::FETCH_NUM);
         $statement->execute($query->getParams());
         return $statement;
@@ -193,7 +207,9 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
 
     protected function generateQuery(Collection $collection)
     {
-        $collections = iterator_to_array(CollectionIterator::recursive($collection), true);
+        $collections = iterator_to_array(
+            CollectionIterator::recursive($collection), true
+        );
         $sql = new Sql;
 
         $this->buildSelectStatement($sql, $collections);
@@ -207,9 +223,11 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
         $primaryName = $this->getStyle()->identifier($collection->getName());
         $cols = get_object_vars($entity);
 
-        foreach ($cols as &$c)
-            if (is_object($c))
+        foreach ($cols as &$c) {
+            if (is_object($c)) {
                 $c = $c->{$primaryName};
+            }
+        }
 
         return $cols;
     }
@@ -223,15 +241,14 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
                     foreach ($columns as $col) {
                         $selectTable[] = "{$tableSpecifier}_mix{$mixin}.$col";
                     }
-                        $selectTable[] = "{$tableSpecifier}_mix{$mixin}." . 
-                        $this->getStyle()->identifier($mixin) . 
-                        " as {$mixin}_id";
+                    $selectTable[] = "{$tableSpecifier}_mix{$mixin}." . 
+                    $this->getStyle()->identifier($mixin) . 
+                    " as {$mixin}_id";
                 }
             }
             if ($this->filterable($c)) {
                 $filters = $this->getFilters($c);
                 if ($filters) {
-                    
                     $pkName = $tableSpecifier . '.' .
                         $this->getStyle()->identifier($c->getName());
                         
@@ -269,47 +286,40 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
         $conditions = $aliases = array();
 
         foreach ($collections as $alias => $collection)
-            $this->parseCollection($sql, $collection, $alias, $aliases, $conditions);
+            $this->parseCollection(
+                $sql, $collection, $alias, $aliases, $conditions
+            );
 
         return $sql->where($conditions);
     }
 
     protected function parseConditions(&$conditions, $collection, $alias)
     {
-        $entity = $collection->getName();
+        $entity             = $collection->getName();
         $originalConditions = $collection->getCondition();
-        $parsedConditions = array();
-        $aliasedPk = $alias . '.' . $this->getStyle()->identifier($entity);
+        $parsedConditions   = array();
+        $aliasedPk          = $this->getStyle()->identifier($entity);
+        $aliasedPk          = $alias . '.' . $aliasedPk;
 
-        if (is_scalar($originalConditions))
+        if (is_scalar($originalConditions)) {
             $parsedConditions = array($aliasedPk => $originalConditions);
-        elseif (is_array($originalConditions))
-            foreach ($originalConditions as $column => $value)
-                if (is_numeric($column))
-                    $parsedConditions[$column] = preg_replace("/{$entity}[.](\w+)/", "$alias.$1", $value);
-                else
+        } elseif (is_array($originalConditions)) {
+            foreach ($originalConditions as $column => $value) {
+                if (is_numeric($column)) {
+                    $parsedConditions[$column] = preg_replace(
+                        "/{$entity}[.](\w+)/", "$alias.$1", $value
+                    );
+                } else {
                     $parsedConditions["$alias.$column"] = $value;
+                }
+            }
+        }
 
         return $parsedConditions;
     }
-
-    protected function parseCollection(Sql $sql, Collection $collection, $alias, &$aliases, &$conditions)
+    
+    protected function parseMixins(Sql $sql, Collection $collection, $entity)
     {
-        $entity = $collection->getName();
-        $parent = $collection->getParentName();
-        $next = $collection->getNextName();
-
-        $parentAlias = $parent ? $aliases[$parent] : null;
-        $aliases[$entity] = $alias;
-        $parsedConditions = $this->parseConditions($conditions, $collection, $alias);
-
-        if (!empty($parsedConditions))
-            $conditions[] = $parsedConditions;
-
-
-        if (is_null($parentAlias)) 
-            $sql->from($entity);
-        
         if ($this->mixable($collection)) {
             foreach ($this->getMixins($collection) as $mix => $spec) {
                 $sql->innerJoin($mix);
@@ -317,158 +327,218 @@ class Mapper extends AbstractMapper implements c\Filterable, c\Mixable, c\Typabl
             }
         }
         
-        if (is_null($parentAlias)) 
-            return $sql;
-        
-        if ($collection->isRequired())
-            $sql->innerJoin($entity);
-        else
-            $sql->leftJoin($entity);
-
-        if ($alias !== $entity)
-            $sql->as($alias);
-            
-        
-        $aliasedPk = $alias . '.' . $this->getStyle()->identifier($entity);
-        $aliasedParentPk = $parentAlias . '.' . $this->getStyle()->identifier($parent);
-
-        if ($entity === $this->getStyle()->composed($parent, $next)
-                || $entity === $this->getStyle()->composed($next, $parent))
-            return $sql->on(
-                array(
-                    $alias . '.' . $this->getStyle()->remoteIdentifier($parent) => $aliasedParentPk
-                )
-            );
-        else
-            return $sql->on(
-                array(
-                    $parentAlias . '.' . $this->getStyle()->remoteIdentifier($entity) => $aliasedPk
-                )
-            );
     }
 
-    protected function fetchSingle(Collection $collection, PDOStatement $statement)
-    {
-        $name = $collection->getName();
-        $entityName = $name;
-        $primaryName = $this->getStyle()->identifier($name);
-        if (!$this->typable($collection)) {
-            $entityClass = $this->entityNamespace . $this->getStyle()->styledName($entityName);
-            $entityClass = class_exists($entityClass) ? $entityClass : '\stdClass';
+    protected function parseCollection(
+        Sql $sql, Collection $collection, $alias, &$aliases, &$conditions
+    ) {
+        $s      = $this->getStyle();
+        $entity = $collection->getName();
+        $parent = $collection->getParentName();
+        $next   = $collection->getNextName();
+
+        $parentAlias = $parent ? $aliases[$parent] : null;
+        $aliases[$entity] = $alias;
+        $conditions = $this->parseConditions(
+            $conditions, 
+            $collection, 
+            $alias
+        ) ?: $conditions;
+
+        //No parent collection means it's the first table in the query
+        if (is_null($parentAlias)) {
+            $sql->from($entity);
+            $this->parseMixins($sql, $collection, $entity);
+            return;
         } else {
-            $entityClass = '\stdClass';
+            
+            if ($collection->isRequired()) {
+                $sql->innerJoin($entity);
+            } else {
+                $sql->leftJoin($entity);
+            }
+            
+            $this->parseMixins($sql, $collection, $entity);
+
+            if ($alias !== $entity) {
+                $sql->as($alias);
+            }
+            
+            $aliasedPk       = $alias . '.' . $s->identifier($entity);
+            $aliasedParentPk = $parentAlias . '.' . $s->identifier($parent);
+
+            if ($this->hasComposition($entity, $next, $parent)) {
+                $onName  = $alias . '.' . $s->remoteIdentifier($parent);
+                $onAlias = $aliasedParentPk;
+            } else {
+                $onName  = $parentAlias . '.' . $s->remoteIdentifier($entity);
+                $onAlias = $aliasedPk;
+            }
+            return $sql->on(array($onName => $onAlias));
         }
-        $statement->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, $entityClass);
-        $row = $statement->fetch();
+    }
+    
+    protected function hasComposition($entity, $next, $parent)
+    {
+        $s = $this->getStyle();
+        return $entity === $s->composed($parent, $next)
+            || $entity === $s->composed($next, $parent);
+    }
 
+    protected function fetchSingle(
+        Collection $collection, PDOStatement $statement
+    ) {
+        $name        = $collection->getName();
+        $entityName  = $name;
+        $primaryName = $this->getStyle()->identifier($name);
+        $row         = $statement->fetch(PDO::FETCH_OBJ);
 
-        if (!$row)
+        if (!$row) {
             return false;
+        }
             
         if ($this->typable($collection)) {
             $entityName = $row->{$this->getType($collection)};
-            $entityClass = $this->entityNamespace . $this->getStyle()->styledName($entityName);
-            $entityClass = class_exists($entityClass) ? $entityClass : '\stdClass';
-            $newRow = new $entityClass;
-            foreach ($row as $prop => $value) {
-                $newRow->$prop = $value;
-            }
-            $row = $newRow;
         }
-
+      
         $entities = new SplObjectStorage();
-        $entities[$row] = $collection;
+        $entities[$this->transformSingleRow($row, $entityName)] = $collection;
 
         return $entities;
     }
-
-    protected function fetchMulti(Collection $collection, PDOStatement $statement)
+    
+    protected function getNewEntityByName($entityName)
     {
+        $entityName = $this->getStyle()->styledName($entityName);
+        $entityClass = $this->entityNamespace . $entityName;
+        $entityClass = class_exists($entityClass) ? $entityClass : '\stdClass';
+        return new $entityClass;
+    }
+    
+    protected function transformSingleRow($row, $entityName)
+    {  
+        $newRow = $this->getNewEntityByName($entityName);
+        
+        foreach ($row as $prop => $value) {
+            $this->inferSet($newRow, $prop, $value);
+        }
+        return $newRow;
+    }
+    
+    protected function inferSet(&$entity, $prop, $value)
+    {
+        $setterName = $this->getSetterStyle($prop);
+        if (method_exists($entity, $setterName)) {
+            $entity->$setterName($value);
+        } else {
+            $entity->$prop = $value;
+        }
+    }
+
+    protected function fetchMulti(
+        Collection $collection, PDOStatement $statement
+    ) {
         $entityInstance = null;
-        $entities = array();
-        $row = $statement->fetch(PDO::FETCH_NUM);
+        $entities       = array();
+        $row            = $statement->fetch(PDO::FETCH_NUM);
 
-        if (!$row)
+        if (!$row) {
             return false;
+        }
+        
+        $this->postHydrate(
+            $entities = $this->createEntities($row, $statement, $collection)
+        );
 
-        $entities = new SplObjectStorage();
+        return $entities;
+    }
+    
+    protected function createEntities(
+        $row, PDOStatement $statement, Collection $collection
+    ) {
+    
+        $entities          = new SplObjectStorage();
+        $entitiesInstances = $this->buildEntitiesInstances(
+            $collection, 
+            $entities
+        );
+        $entityInstance    = array_pop($entitiesInstances);
+        
+        //Reversely traverses the columns to avoid conflicting foreign key names
+        foreach (array_reverse($row, true) as $col => $value) {
+            $columnMeta    = $statement->getColumnMeta($col);
+            $columnName    = $columnMeta['name'];
+            $primaryName   = $this->getStyle()->identifier(
+                $entities[$entityInstance]->getName()
+            );
+            
+            $this->inferSet($entityInstance, $columnName, $value);
+                
+            if ($primaryName == $columnName) {
+                $entityInstance = array_pop($entitiesInstances);
+            }
+        }
+        return $entities;
+    }
+    
+    protected function buildEntitiesInstances(
+        Collection $collection, SplObjectStorage $entities
+    ) {
         $entitiesInstances = array();
-
+        
         foreach (CollectionIterator::recursive($collection) as $c) {
-            if ($this->filterable($c)) {
-                $filters = $this->getFilters($c);
-                if (!$filters) {
-                    continue;
-                }
+            if ($this->filterable($c) && !$this->getFilters($c)) {
+                continue;
             }
-            $tableName = $c->getName();
-            $entityName = $this->getStyle()->styledName($tableName);
-            if (!$this->typable($c)) {
-                $entityClass = $this->entityNamespace . $entityName;
-                $entityClass = class_exists($entityClass) ? $entityClass : 'stdClass';
-            } else {
-                $entityClass = 'stdClass';
-            }
-            $entityInstance = new $entityClass;
+            
+            $entityInstance = $this->getNewEntityByName($c->getName());
             $mixins = array();
+            
             if ($this->mixable($c)) {
                 $mixins = $this->getMixins($c);
                 foreach ($mixins as $mix) {
                     $entitiesInstances[] = $entityInstance;
                 }
             }
+            
             $entities[$entityInstance] = $c;
             $entitiesInstances[] = $entityInstance;
         }
-
-        $entityInstance = array_pop($entitiesInstances);
-        
-        //Reversely traverses the columns to avoid conflicting foreign key names
-        foreach (array_reverse($row, true) as $col => $value) {
-            $entityData = $entities[$entityInstance];
-            $columnMeta = $statement->getColumnMeta($col);
-            $columnName = $columnMeta['name'];
-            $setterName = $this->getSetterStyle($columnName);
-            $primaryName = $this->getStyle()->identifier($entityData->getName());
-            
-            if (method_exists($entityInstance, $setterName))
-                $entityInstance->$setterName($value);
-            else
-                $entityInstance->$columnName = $value;
-                
-            if ($primaryName == $columnName) 
-                $entityInstance = array_pop($entitiesInstances);
-        }
-        
-
+        return $entitiesInstances;
+    }
+    
+    protected function postHydrate(SplObjectStorage $entities)
+    {
         $entitiesClone = clone $entities;
 
         foreach ($entities as $instance) {
             foreach ($instance as $field => &$v) {
                 if ($this->getStyle()->isRemoteIdentifier($field)) {
                     foreach ($entitiesClone as $sub) {
-                        $tableName = $entities[$sub]->getName();
-                        $primaryName = $this->getStyle()->identifier($tableName);
-                        
-                        if ($tableName === $this->getStyle()->remoteFromIdentifier($field)
-                                && $sub->{$primaryName} === $v) {
-                            $v = $sub;
-                        }
+                        $this->tryHydration($entities, $sub, $field, $v);
                     }
                 }
             }
         }
-
-        return $entities;
+    }
+    
+    protected function tryHydration($entities, $sub, $field, &$v)
+    {
+        $tableName = $entities[$sub]->getName();
+        $primaryName = $this->getStyle()->identifier($tableName);
+        
+        if ($tableName === $this->getStyle()->remoteFromIdentifier($field)
+                && $sub->{$primaryName} === $v) {
+            $v = $sub;
+        }
     }
 
     protected function getSetterStyle($name)
     {
-        $name = ucfirst(str_replace('_', '', $this->getStyle()->styledProperty($name)));
+        $name = str_replace('_', '', $this->getStyle()->styledProperty($name));
         return "set{$name}";
     }
 
-    
     public function getFilters(Collection $collection)
     {
         return $collection->getExtra('filters');
